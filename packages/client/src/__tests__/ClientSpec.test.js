b6d041aef892e3c1e0b46b88b69d85e59416b23b
Lee Hoyong <igotoweb@gmail.com>
2020-11-24 21:14:07 +0900

    make test works independently

--- packages/client/src/__tests__/ClientSpec.test.js
+++ packages/client/src/__tests__/ClientSpec.test.js
@@ -6,46 +6,8 @@ import MovieRankList from '../MovieRankList';
 import CurrentMovie from '../CurrentMovie';
 import App from '../App';
 
-const mockMovie = [
-  {
-    id: 5511,
-    url: 'https://yts.lt/movie/deadpool-2016',
-    title: 'Deadpool',
-    year: 2017,
-    rating: 8.6,
-    runtime: 107,
-    genres: ['Action', 'Adventure', 'Comedy', 'Romance', 'Sci-Fi', 'Mock'],
-    summary:
-      'This is the origin story of former Special Forces operative turned mercenary Wade Wilson, who after being subjected to a rogue experiment that leaves him with accelerated healing powers, adopts the alter ego Deadpool. Armed with his new abilities and a dark, twisted sense of humor, Deadpool hunts down the man who nearly destroyed his life!',
-    description_full:
-      'This is the origin story of former Special Forces operative turned mercenary Wade Wilson, who after being subjected to a rogue experiment that leaves him with accelerated healing powers, adopts the alter ego Deadpool. Armed with his new abilities and a dark, twisted sense of humor, Deadpool hunts down the man who nearly destroyed his life!',
-    small_cover_image:
-      'https://yts.lt/assets/images/movies/deadpool_2016/small-cover.jpg',
-    medium_cover_image:
-      'https://yts.lt/assets/images/movies/deadpool_2016/medium-cover.jpg',
-    large_cover_image:
-      'https://yts.lt/assets/images/movies/deadpool_2016/large-cover.jpg',
-  },
-  {
-    id: 1266,
-    url: 'https://yts.lt/movie/furious-7-2015',
-    title: 'Furious 7',
-    year: 2016,
-    rating: 7.1,
-    runtime: 136,
-    genres: ['Action', 'Adventure', 'Crime', 'Thriller', 'Mock'],
-    summary:
-      "Dominic and his crew thought they'd left the criminal mercenary life behind. They'd defeated international terrorist Owen Shaw and went their separate ways. But now, Shaw's brother, Deckard Shaw, is out killing the crew one by one for revenge. Worse, a Somalian terrorist called Jakarde and a shady government official called \"Mr. Nobody\" are both competing to steal a computer terrorism program called \"God's Eye,\" that can turn any technological device into a weapon. Torretto must reconvene with his team to stop Shaw and retrieve the God's Eye program while caught in a power struggle between the terrorist and the United States government!",
-    description_full:
-      "Dominic and his crew thought they'd left the criminal mercenary life behind. They'd defeated international terrorist Owen Shaw and went their separate ways. But now, Shaw's brother, Deckard Shaw, is out killing the crew one by one for revenge. Worse, a Somalian terrorist called Jakarde and a shady government official called \"Mr. Nobody\" are both competing to steal a computer terrorism program called \"God's Eye,\" that can turn any technological device into a weapon. Torretto must reconvene with his team to stop Shaw and retrieve the God's Eye program while caught in a power struggle between the terrorist and the United States government!",
-    small_cover_image:
-      'https://yts.lt/assets/images/movies/furious_seven_2015/small-cover.jpg',
-    medium_cover_image:
-      'https://yts.lt/assets/images/movies/furious_seven_2015/medium-cover.jpg',
-    large_cover_image:
-      'https://yts.lt/assets/images/movies/furious_seven_2015/large-cover.jpg',
-  },
-];
+// 테스트 통과를 위해 mockup 데이터를 참고하세요.
+import mockMovie from '../mockMovie';
 
 describe('MovieRankListEntry test', () => {
   let container;
@@ -217,8 +179,6 @@ describe('CurrentMovie test', () => {
 
 describe('App test', () => {
   let container;
-  let spy;
-  let fetchSpy;
   let mockApp;
 
   beforeAll(async () => {
@@ -226,15 +186,6 @@ describe('App test', () => {
     document.body.appendChild(container);
 
     mockApp = new App();
-
-    spy = jest.spyOn(App.prototype, 'handleCardClick');
-    fetchSpy = jest.spyOn(global, 'fetch');
-
-    fetchSpy.mockImplementation(() =>
-      Promise.resolve({
-        json: () => Promise.resolve(mockMovie),
-      })
-    );
 
     await act(async () => {
       ReactDOM.render(<App />, container);
@@ -255,7 +206,13 @@ describe('App test', () => {
     expect(typeof mockApp.handleCardClick).toEqual('function');
   });
 
-  test('handleCardClick 메서드를 실행하면 `setState` 함수가 실행되어야 합니다.', () => {
+  test('App 컴포넌트는 현재 영화 정보(currentMovie)와 영화 리스트(movies)를 `state`로 가지고 있어야 합니다.', () => {
+    expect(mockApp.state.currentMovie).not.toEqual(undefined);
+    expect(mockApp.state.movies).not.toEqual(undefined);
+  });
+
+  test('영화 목록을 클릭할 때에, `handleCardClick` 메서드를 실행되고, 상태가 변화되어야 합니다.', () => {
+    const spy = jest.spyOn(App.prototype, 'handleCardClick');
     const stateSpy = jest.spyOn(App.prototype, 'setState');
     const cards = container.querySelectorAll('.card');
     cards[0].dispatchEvent(new MouseEvent('click', { bubbles: true }));
@@ -266,12 +223,8 @@ describe('App test', () => {
     spy.mockClear();
   });
 
-  test('App 컴포넌트는 현재 영화 정보(currentMovie)와 영화 리스트(movies)를 `state`로 가지고 있어야 합니다.', () => {
-    expect(mockApp.state.currentMovie).not.toEqual(undefined);
-    expect(mockApp.state.movies).not.toEqual(undefined);
-  });
-
   test('영화 목록을 클릭할 때에, `handleCardClick` 메서드를 이용해서 현재 영화정보를 업데이트해야 합니다.', () => {
+    const spy = jest.spyOn(App.prototype, 'handleCardClick');
     const cards = container.querySelectorAll('.card');
     cards[1].dispatchEvent(new MouseEvent('click', { bubbles: true }));
 
